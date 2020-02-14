dceb7c9d2d0c9d6a0f366d044881299fd3b9fae2
Linh Le <ltt.lop9a1.lhlinh@gmail.com>
2020-02-14 22:24:59 +0700

    Add test.js file

---
+++ test.js
@@ -0,0 +1,11 @@
+
+var array = [0, 1, 2, 3, 4]
+
+var newarray = array.slice(0, array.length)
+newarray[0] = 10
+
+console.log(array);
+console.log(newarray);
+
+// console.log(array);
+// console.log(newarray);
